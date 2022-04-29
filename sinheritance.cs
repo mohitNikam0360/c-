26e242c08b81f96110e47cdf7df0fc212e2532aa
mohitNikam0360 <104616482+mohitNikam0360@users.noreply.github.com>
2022-04-29 16:37:03 +0530

    Create sinheritance.cs

---
+++ sinheritance.cs
@@ -0,0 +1,25 @@
+# Program To demonstrate simple inheritance in c#.net.
+using System;
+Namespace CSharp_Shell
+{
+Public class Program 
+    {
+    Public	int rollno=107;
+     Public string	name=”harshali”;
+    }
+    Class exam:Program
+    {
+  Public static void Main()
+        {
+         	Exam n=new exam();
+           Console.WriteLine(n.rollno);
+           Console.WriteLine( n.name);
+        	Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
