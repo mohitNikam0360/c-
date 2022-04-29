9f9357c9c43c5b9ddd9cb486e7518d2b4347cdab
mohitNikam0360 <104616482+mohitNikam0360@users.noreply.github.com>
2022-04-29 16:47:56 +0530

    Create pfunction.cs

---
+++ pfunction.cs
@@ -0,0 +1,17 @@
+# program to demonstrate  parameterize function in c#.net.
+using System;
+namespace CSharp_Shell
+{
+           Public class Program 
+    {  
+    Int x=0;
+    Void print(int x,int y)
+    	{
+    		x=x+y;
+    	Console.WriteLine(x);
+    	}
+        Public static void Main() 
+     {
+        	Program p=new Program();
+           p.print(5,5);
+           }     }    }
