f35128ca4512af8d48c1b7648e4615db0f93367d
mohitNikam0360 <104616482+mohitNikam0360@users.noreply.github.com>
2022-04-29 16:46:50 +0530

    Create fcall.cs

---
+++ fcall.cs
@@ -0,0 +1,15 @@
+# Program to demonstrate function definition and function call in c#.net.
+using System;
+Namespace CSharp_Shell
+{
+  Public class Program 
+    {  
+     Void print()
+    	{
+    	Console.WriteLine(“Harshali”);
+    	}
+        Public static void Main() 
+        {
+        	Program p=new Program();
+           p.print();
+        }    }  }
