9745eafb9f226e1340ab9d020c1a43c4e5968014
mohitNikam0360 <104616482+mohitNikam0360@users.noreply.github.com>
2022-04-29 16:33:16 +0530

    Create teach.cs

---
+++ teach.cs
@@ -0,0 +1,18 @@
+# Program to print “Teach one,Each one,Tree one” for given number of times.
+using System;
+public class While
+	{
+                public static void Main()
+		{
+			int i;
+			int n;
+			Console.WriteLine("Enter A Number:- ");
+			n = Convert.ToInt32(Console.ReadLine());
+     			for(i=1;i<=n;i++)
+			{
+				Console.WriteLine("Teach One,Each One,Tree One");
+				}
+			Console.ReadLine();
+			}
+	}
+
