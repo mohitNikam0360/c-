f449c6f99b88a5557b3f45fe7f58750fdd76c944
mohitNikam0360 <104616482+mohitNikam0360@users.noreply.github.com>
2022-04-30 08:56:47 +0530

    Create functionoverriding.cs

---
+++ functionoverriding.cs
@@ -0,0 +1,28 @@
+\\ program to show function overriding in c#.net.
+
+Using System;
+Namespace CSharp_Shell
+{
+    Public class Program 
+    {
+    	Public virtual void showinfo()
+    	{
+    		Console.WriteLine(“base class method”);
+    	}
+    }
+    Class b:Program
+    {
+    	Public override void showinfo()
+    	{
+    		Console.WriteLine(“derived class method”);
+    	}
+    
+        Public static void Main()
+        {
+			B b1=new b();
+			B1.showinfo();
+			B1.showinfo();
+        }
+    }
+}
+
