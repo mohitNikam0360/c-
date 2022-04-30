f945d560a3537329ccff5fd92543d563f82260ed
mohitNikam0360 <104616482+mohitNikam0360@users.noreply.github.com>
2022-04-30 08:59:27 +0530

    Create  exceptionhandling.cs

---
+++ exceptionhandling.cs
@@ -0,0 +1,24 @@
+\\Program to demonstrate exception handling in c#.net.
+
+Using System;
+namespace CSharp_Shell
+{	
+ Public class Program 
+    {
+ Public static void Main()
+   {
+Try
+	{
+  	Int a=2;
+   	Int b=0;
+    	Int c=a/b;
+  	Console.WriteLine(c);
+ 	}
+   Catch(Exception e)
+	{
+	Console.WriteLine(“Exception occurred:-“+e);
+	}
+  Finally
+	{
+	Console.WriteLine(“Exception handled”);
+	}       }        }     }
